da04873cce62f0aa8caeb3973d3e49ecffc0ed25
~Armin~ <62879887+ArsamCodex@users.noreply.github.com>
2024-04-15 03:59:49 +0100

    Update TweetController.cs

--- Controllers/TweetController.cs
+++ Controllers/TweetController.cs
@@ -13,10 +13,10 @@ namespace TwitterApi.Controllers
         [HttpPost]
         public async Task< IActionResult> PosterT(TweetModelDto tweetModelDto)
         {
-            var client = new TwitterClient("nDi7ms0i7HZhbYnXqoTt6NtUg",
-                "VMAsiZNIs7yOnnjHjbnLRmQMlP6bwUTE29UVnA3QMN3YSu6xZi",
-                "1525161521713668096-fJbaJFX0vVMRqAU0fovCAZD3l33t7m",
-                "albuYF5RyHFI0WRuJCON88DuyiZgzzhmUMRmUzmysQqVI");
+            var client = new TwitterClient("",
+                "",
+                "",
+                "ap");
 
             var result = await client.Execute.AdvanceRequestAsync(GoForRequyuest(tweetModelDto, client));
             return Ok(result.Content);
